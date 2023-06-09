94c9b617c5baa4eb44eb8f99099af103ee294205
rishabhjain12 <rishabhj.1222@gmail.com>
2023-06-09 16:46:39 +0530

    working on footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,32 @@
-import "./Footer.scss";
+import React from 'react'
+import './Footer.scss'
+// import { FaLocationArrow, FaMobileAlt, FaEnvelope } from 'react-icons/fa'
+// import Payment from '../../assets/payments.png'
 const Footer = () => {
-    return <div>Footer</div>;
-};
+  return (
+    <Footer className='footer'>
+      <div className='footer-content'>
+        <div className='col'>
+          <div className='title'>About</div>
+          <div className='text'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            dolor consequuntur dolore nesciunt nobis eius soluta impedit. Fugit
+            soluta magnam porro facere repudiandae deleniti, at animi.
+            Voluptatem temporibus quidem expedita.
+          </div>
+        </div>
+        <div className='col'>
+          <div className='title'>Contact</div>
+        </div>
+        <div className='col'>
+          <div className='title'>Categories</div>
+        </div>
+        <div className='col'>
+          <div className='title'>Pages</div>
+        </div>
+      </div>
+    </Footer>
+  )
+}
 
-export default Footer;
+export default Footer
